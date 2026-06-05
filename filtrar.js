74bf35ec01ce10ba4125300ae4272e0a0a028203
TelHUOP <salatielhuop@gmail.com>
2026-06-05 16:38:46 -0300

    ajuste

---
+++ filtrar.js
@@ -0,0 +1,12 @@
+fetch('table')
+.then
+
+
+<div class="caixa-maior">
+        <input class="input-digite" placeholder="digite">
+        <button class="botao">
+            <img src="./img/lupa.png" alt="buscar" class="img-botao"> 
+        </button>
+    </div>
+    <script src="filtrar.js"></script>
+    
